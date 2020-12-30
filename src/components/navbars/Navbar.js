56dd820da38d84cc3830280472cff0095815793d
Mohannad Zitoun <mzitoun@hotmail.com>
2020-12-30 22:01:47 +0100

    add hamburger menu

--- src/components/navbars/Navbar.js
+++ src/components/navbars/Navbar.js
@@ -1,11 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Navbar.css';
+import Hamburger from 'hamburger-react';
 
 const Navbar = () => {
+  const [open, setOpen] = useState(false);
   return (
     <div className='navbar'>
+      <div className='navbar__header'>
+        <a href='#'>
+          <img
+            src='https://images.unsplash.com/photo-1609252871434-4e282b868d9a?ixid=MXwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxMHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=400&q=60'
+            alt='My Image'
+          />
+        </a>
+      </div>
+      <button onClick={() => setOpen(!open)} className='navbar__hamburger'>
+        <Hamburger />
+      </button>
       <div className='navbar__nav'>
-        <ul>
+        <ul className={`${open ? 'active' : 'inactive'} `}>
           <li>
             <a href='#home'>
               <span className='navbar__nav-number'>01.</span>Home
