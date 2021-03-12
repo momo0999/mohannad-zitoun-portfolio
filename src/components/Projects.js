0d5639c95b5173338d1860e68f03e4fdc9e23ff6
Mohannad Zitoun <mzitoun@hotmail.com>
2021-03-12 15:37:39 +0100

    refactor Projects component

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,8 +1,37 @@
-import React from 'react';
-import projectPortfolio from '../image/projectPortfolio.png';
+import React, { Fragment } from 'react';
+import projects from '../projects';
 import './Projects.css';
 
 const Projects = () => {
+  const renderedProjects = projects.map(
+    ({ name, img, desc, githubLink, websiteLink, technologies }) => {
+      return (
+        <Fragment>
+          <div className='project__card'>
+            <div className='project__card-row'>
+              <i className='far fa-folder'></i>
+
+              <div>
+                <a rel='noreferrer' target='_blank' href={websiteLink}>
+                  <i className='fas fa-external-link-alt'></i>
+                </a>
+                <a rel='noreferrer' target='_blank' href={githubLink}>
+                  <i className='fab fa-github'></i>
+                </a>
+              </div>
+            </div>
+            <h1>{name}</h1>
+            <p>{desc}</p>
+            <div className='project__card-rowTech'>
+              {technologies.map((tech) => {
+                return <p className='tech'>{tech}</p>;
+              })}
+            </div>
+          </div>
+        </Fragment>
+      );
+    }
+  );
   return (
     <div className='projects'>
       <div className='projects__header'>
@@ -11,30 +40,7 @@ const Projects = () => {
         </h1>
         <div className='header-line'></div>
       </div>
-      <div className='projects__col'>
-        <div className='project__container'>
-          <div className='project__container-image'>
-            <div className='project__container-imageLayer'></div>
-            <img src={projectPortfolio} alt='portfolio' />
-          </div>
-          <div className='project__container-content'>
-            <p>Featured Project</p>
-            <h1>Portfolio</h1>
-            <div className='project__container-content__textBox'>
-              <p>My portfolio website I built using HTML, CSS and React js</p>
-            </div>
-            <div className='projects__links'>
-              <a
-                rel='noreferrer'
-                target='_blank'
-                href='https://github.com/momo0999/mohannad-zitoun-portfolio'
-              >
-                <i className='fab fa-github'></i>
-              </a>
-            </div>
-          </div>
-        </div>
-      </div>
+      <div className='projects__col'>{renderedProjects}</div>
     </div>
   );
 };
---
+++ src/projects.js
@@ -0,0 +1,30 @@
+import projectPortfolio from './image/projectPortfolio.png';
+import myBlog from './image/myBlog.png';
+const projects = [
+  {
+    name: 'Portfolio',
+    desc: 'This my latest portfolio website where you are right now. ',
+    img: projectPortfolio,
+    githubLink: 'https://github.com/momo0999/mohannad-zitoun-portfolio',
+    websiteLink: 'https://www.mohannadzitoun.com/',
+    technologies: ['HTML', 'CSS', 'JavaScript', 'React'],
+  },
+  {
+    name: 'Blog',
+    desc:
+      'This is a blog and a photo gallery where the admin can manage and update the content.',
+    img: myBlog,
+    githubLink: 'https://github.com/momo0999/blog-mern',
+    websiteLink: 'https://mernstack-blog.herokuapp.com/',
+    technologies: [
+      'HTML',
+      'CSS',
+      'JavaScript',
+      'React.js',
+      'Node.js',
+      'MongoDB',
+    ],
+  },
+];
+
+export default projects;
